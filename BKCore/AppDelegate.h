64103d65fc3f8a00a9ceacc9d8e3937953cca332
terry.guo <terry.guo@mei.com>
2016-08-19 14:14:47 +0800

    init

---
+++ BKCore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BKCore
+//
+//  Created by terry.guo on 16/8/19.
+//  Copyright © 2016年 Bangko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BKCore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BKCore
+//
+//  Created by terry.guo on 16/8/19.
+//  Copyright © 2016年 Bangko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
